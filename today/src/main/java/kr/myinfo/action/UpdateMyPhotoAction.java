c2f759a78b4a9907302021f34d05117b6275c729
ssy2253 <88231613+ssy2253@users.noreply.github.com>
2023-01-05 09:36:41 +0900

    properties수정, myinfo action 생성 및 관련 jsp 생성

---
+++ today/src/main/java/kr/myinfo/action/UpdateMyPhotoAction.java
@@ -0,0 +1,16 @@
+package kr.myinfo.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.controller.Action;
+
+public class UpdateMyPhotoAction implements Action{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		return "/WEB-INF/views/common/ajax_view.jsp";
+	}
+
+}
