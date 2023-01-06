003fe4d16b48202959f5e3e3eb5d7687b41071e6
phseon <88360147+phseon@users.noreply.github.com>
2023-01-07 03:37:35 +0900

    리뷰 작성 폼 생성, properties 작성

--- today/src/main/java/kr/review/action/WriteFormAction.java
+++ today/src/main/java/kr/review/action/WriteFormAction.java
@@ -11,14 +11,13 @@ public class WriteFormAction implements Action{
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		HttpSession session = request.getSession();
-//		Integer m_num =
-//				(Integer)session.getAttribute("m_num");
-//		if(m_num==null) {//로그인이 되지 않은 경우
-			return "redirect:/main/main.do";
-//			return "/WEB-INF/views/main/main.jsp";
-//		}
+		Integer m_num =
+				(Integer)session.getAttribute("m_num");
+		if(m_num==null) {//로그인이 되지 않은 경우
+			return "redirect:/member/loginForm.do";
+		}
 		//로그인 된 경우
-//		return "/WEB-INF/views/review/writeForm.jsp";
+		return "/WEB-INF/views/review/writeForm.jsp";
 	}
 
 }
