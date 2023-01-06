2fa87f0fa92c3afe89e0ce14c517d4c41c07bc2d
ssy2253 <88231613+ssy2253@users.noreply.github.com>
2023-01-06 13:07:13 +0900

    리뷰list, 질문list, 예약list 조회 action 및 jsp 생성, 프로퍼티스 수정

---
+++ today/src/main/java/kr/myinfo/action/ReservationInfoListAction.java
@@ -0,0 +1,18 @@
+package kr.myinfo.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.controller.Action;
+
+public class ReservationInfoListAction implements Action {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		
+		
+		return "/WEB-INF/views/myinfo/reservationInfoList.jsp";
+	}
+
+}
