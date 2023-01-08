3ec8a7073f8d2e8f62e7f333c11b35ede08a1086
ssy2253 <88231613+ssy2253@users.noreply.github.com>
2023-01-08 20:33:52 +0900

    마이페이지 유저정보수정 테스트 및 로그인처리 부분 수정

--- today/src/main/java/kr/myinfo/action/ModifyUserFormAction.java
+++ today/src/main/java/kr/myinfo/action/ModifyUserFormAction.java
@@ -12,18 +12,17 @@ public class ModifyUserFormAction implements Action{
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-	/*	
+	
 		// 세션 여부 확인하기
 		HttpSession session = request.getSession();
 		Integer user_num = (Integer)session.getAttribute("user_num");
 		
 		if(user_num==null) {
-			return "";
+			return "redirect:/member/login.do";
 		}
-		*/
+		
 		MyInfoDAO dao = MyInfoDAO.getInstance();
-		//MemberVO member = dao.getMemberInfo(user_num);
-		MemberVO member = dao.getMemberInfo(1); // getMemberInfo메서드로 유저에 대한 정보를 form에 전달하기
+		MemberVO member = dao.getMemberInfo(user_num);
 		request.setAttribute("member", member);
 		
 		
