7f34ffc2e2fd1f4641161925d91a1cbfa3bf4c22
ch00 <ch00@chooo>
2023-01-10 16:11:20 +0900

    의사VO 추가

---
+++ today/src/main/java/kr/doctor/vo/DoctorVO.java
@@ -0,0 +1,34 @@
+package kr.doctor.vo;
+
+public class DoctorVO {
+	private String name;
+	private String imgsrc;
+	private String content;
+	private int d_num;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getImgsrc() {
+		return imgsrc;
+	}
+	public void setImgsrc(String imgsrc) {
+		this.imgsrc = imgsrc;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getD_num() {
+		return d_num;
+	}
+	public void setD_num(int d_num) {
+		this.d_num = d_num;
+	}
+	
+}
