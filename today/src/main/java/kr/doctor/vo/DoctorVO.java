12b4464cd9ba52b02505d5e77d2e1d9b6c0c6854
ssy2253 <88231613+ssy2253@users.noreply.github.com>
2023-01-10 17:31:34 +0900

    시술페이지 VO 수정

--- today/src/main/java/kr/doctor/vo/DoctorVO.java
+++ today/src/main/java/kr/doctor/vo/DoctorVO.java
@@ -1,34 +1,40 @@
 package kr.doctor.vo;
 
 public class DoctorVO {
-	private String p_num;
-	private String imgsrc;
-	private String content;
 	private int m_num;
-	
-	public String getP_num() {
-		return p_num;
-	}
-	public void setP_num(String p_num) {
-		this.p_num = p_num;
-	}
-	public String getImgsrc() {
-		return imgsrc;
-	}
-	public void setImgsrc(String imgsrc) {
-		this.imgsrc = imgsrc;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
+	private int p_num;
+	private String p_title;
+	private String p_content;
+	private String p_imgsrc;
 	public int getM_num() {
 		return m_num;
 	}
 	public void setM_num(int m_num) {
 		this.m_num = m_num;
+	}
+	public String getP_title() {
+		return p_title;
+	}
+	public void setP_title(String p_title) {
+		this.p_title = p_title;
+	}
+	public String getP_content() {
+		return p_content;
+	}
+	public void setP_content(String p_content) {
+		this.p_content = p_content;
+	}
+	public String getP_imgsrc() {
+		return p_imgsrc;
+	}
+	public void setP_imgsrc(String p_imgsrc) {
+		this.p_imgsrc = p_imgsrc;
+	}
+	public int getP_num() {
+		return p_num;
+	}
+	public void setP_num(int p_num) {
+		this.p_num = p_num;
 	}
 	
 }
